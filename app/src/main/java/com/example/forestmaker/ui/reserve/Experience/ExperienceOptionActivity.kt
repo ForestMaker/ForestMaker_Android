fa88106d9405a01e0cff33c9baf8c4e74e4842fc
JiHyeonMon <jhk8806@naver.com>
2021-07-10 17:40:03 +0900

    [Experience] ExperienceOption수정, 예약 버튼 만들기

--- app/src/main/java/com/example/forestmaker/ui/reserve/Experience/ExperienceOptionActivity.kt
+++ app/src/main/java/com/example/forestmaker/ui/reserve/Experience/ExperienceOptionActivity.kt
@@ -33,10 +33,12 @@ class ExperienceOptionActivity : AppCompatActivity() {
 
     fun loadData() {
         experienceOptionAdapter.datas.apply {
-            add("숲속에서 내가 좋아하는 동물찾기")
-            add("숲속에 사는 식물 관찰하기")
-            add("꽃으로 왕관 만들기")
-
+            add("숲 전문가와 함께하는 둘레길 숲 해설")
+            add("피톤치드 뿜뿜 나무 공부하기")
+            add("엄마와 아이랑 같이 하는 명상 체험")
+            add("우리 숲에 살고 있는 동·식물 소개")
+            add("스트레스 날릴 수 있는 힐링 체험")
+            add("맨발로 흙 걷기 체험")
         }
 
         experienceOptionAdapter.notifyDataSetChanged()
