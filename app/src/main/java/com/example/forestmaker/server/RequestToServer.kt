a42bad6879f937a0a586a962e393567e0c1d4f81
JiHyeonMon <jhk8806@naver.com>
2021-07-13 13:49:49 +0900

    [API 버전 맞추기] Android 30에서 okhttp 동작 안하는 라이브러리 존재 - 수정

--- app/src/main/java/com/example/forestmaker/server/RequestToServer.kt
+++ app/src/main/java/com/example/forestmaker/server/RequestToServer.kt
@@ -14,16 +14,17 @@ object RequestToServer {
     var BASE_URL = "http://211.208.228.52:9872"
     var test = "http://10.0.2.2:9872/"
 
-    val loggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    val clientBuilder = OkHttpClient.Builder().addInterceptor(loggingInterceptor)
+//    val loggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+//    val clientBuilder = OkHttpClient.Builder().addInterceptor(loggingInterceptor)
 
     val gson: Gson = GsonBuilder().setLenient().create()
 
     var retrofit: Retrofit =
-        Retrofit.Builder().baseUrl(BASE_URL)
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(clientBuilder.build())
+//            .client(clientBuilder.build())
             .build()
 
     var service: RequestInterface = retrofit.create(RequestInterface::class.java)
