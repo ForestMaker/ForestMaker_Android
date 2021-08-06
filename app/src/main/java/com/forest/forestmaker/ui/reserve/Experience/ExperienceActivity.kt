149cfbf0efc3f36085d28e2d262bf8d982b5ac6d
JiHyeonMon <jhk8806@naver.com>
2021-08-06 13:49:37 +0900

    [fix] Remove GongBang Data in ForestSchoolDetail View

--- app/src/main/java/com/forest/forestmaker/ui/reserve/Experience/ExperienceActivity.kt
+++ app/src/main/java/com/forest/forestmaker/ui/reserve/Experience/ExperienceActivity.kt
@@ -3,8 +3,6 @@ package com.forest.forestmaker.ui.reserve.Experience
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -12,25 +10,16 @@ import com.bumptech.glide.request.RequestOptions
 import com.forest.forestmaker.R
 import com.forest.forestmaker.data.BannerData
 import com.forest.forestmaker.data.ShoppingCartData
-import com.forest.forestmaker.server.RequestToServer
 import com.forest.forestmaker.server.data.ForestSchool
-import com.forest.forestmaker.server.data.GongBangResponse
-import com.forest.forestmaker.ui.reserve.Experience.gongbang.GongBangActivity
 import com.forest.forestmaker.ui.reserve.ShoppingCartAdapter
-import com.forest.forestmaker.ui.reserve.ShoppingCartViewHolder
 import kotlinx.android.synthetic.main.activity_experience.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class ExperienceActivity : AppCompatActivity() {
 
     var recycleData = mutableListOf<BannerData>()
     var forestSchoolDummy = ArrayList<ForestSchool>()
-    var gongbangData = ArrayList<GongBangResponse>()
 
     lateinit var shoppingCartAdapter: ShoppingCartAdapter
-    lateinit var recycleAdapter: RecycleAdapter
     var userEmail = ""
     var position = 0
 
@@ -41,9 +30,6 @@ class ExperienceActivity : AppCompatActivity() {
         setIntentData()
         setForestSchool()
         setButton()
-        setAdapter()
-
-        getGongBangData()
 
     }
 
@@ -79,84 +65,6 @@ class ExperienceActivity : AppCompatActivity() {
             startActivity(intent)
             return@setOnClickListener
         }
-    }
-
-    private fun setAdapter() {
-        shoppingCartAdapter =
-            ShoppingCartAdapter(this, object : ShoppingCartViewHolder.onClickListener {
-                override fun onClickItemDelete(position: Int) {}
-                override fun onPlusItem(position: Int) {}
-                override fun onMinusItem(position: Int) {}
-            })
-
-        recycleAdapter = RecycleAdapter(
-            this,
-            object : RecycleViewHolder.OnClickListener {
-                override fun clickItem(position: Int) {
-
-                    val intent = Intent(this@ExperienceActivity, GongBangActivity::class.java)
-                    intent.putExtra("gongbangList", gongbangData)
-                    intent.putExtra("position", position)
-                    intent.putExtra("user_email", userEmail)
-                    startActivity(intent)
-                }
-            }
-        )
-
-        act_experience_recycler_recyclerview.adapter = recycleAdapter
-        act_experience_recycler_recyclerview.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
-    }
-
-    fun getGongBangData() {
-        RequestToServer.service.requestGongbang()
-            .enqueue(object : Callback<ArrayList<GongBangResponse>> {
-                override fun onResponse(
-                    call: Call<ArrayList<GongBangResponse>>,
-                    response: Response<ArrayList<GongBangResponse>>
-                ) {
-                    if (response.isSuccessful) {
-
-                        for (item in response.body()!!) {
-                            recycleData.apply {
-                                add(
-                                    BannerData(
-                                        bannerImg = item.img_list[0]
-                                    )
-                                )
-                            }
-
-                            gongbangData.apply {
-                                add(
-                                    GongBangResponse(
-                                        name = item.name,
-                                        description = item.description,
-                                        address = item.address,
-                                        hours = item.hours,
-                                        runtime = item.runtime,
-                                        participants = item.participants,
-                                        fee = item.fee,
-                                        fee_int = item.fee_int,
-                                        img_list = item.img_list
-                                    )
-                                )
-                            }
-
-                        }
-
-                        recycleAdapter.datas = recycleData
-                        recycleAdapter.notifyDataSetChanged()
-                    }
-                }
-
-                override fun onFailure(call: Call<ArrayList<GongBangResponse>>, t: Throwable) {
-                    Log.e("fail", t.message.toString())
-                }
-
-            })
-
-
     }
 
     private fun setForestSchool() {
